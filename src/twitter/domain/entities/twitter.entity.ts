19ed4dba07c6b6c4cfebd2fbc2e71574769c5bdd
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2026-02-28 11:34:16 -0300

    feat: ✨ add tweet media and user data

--- src/twitter/domain/entities/twitter.entity.ts
+++ src/twitter/domain/entities/twitter.entity.ts
@@ -1,7 +1,15 @@
+type TweetUser = {
+  profileImageUrl: string;
+  name: string;
+  username: string;
+};
+
 export type TweetEntity = {
   postId: string;
   text: string;
   likesCount: number;
   impressionCount: number;
+  mediaUrl: string | undefined;
+  user: TweetUser;
   createdAt: Date;
 };
