d5ca243870ce327f5c13154cda01ae1afc35127a
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2026-02-28 11:36:30 -0300

    feat: ✨ add extra information to the tweet

--- src/twitter/infrastructure/mappers/tweet-post.mapper.ts
+++ src/twitter/infrastructure/mappers/tweet-post.mapper.ts
@@ -1,14 +1,26 @@
 import type { TweetEntity } from '@twitter/domain/entities';
-import type { Tweet } from './types';
+import type { Tweet, TweetIncludes } from './types';
 
 export class TweetPostMapper {
-  static toDomain(input: Tweet): TweetEntity {
+  static toDomain(tweet: Tweet, includes: TweetIncludes): TweetEntity {
+    const tweetText = tweet.text.replace(/\s*https:?\/\/\S+/g, '');
+
+    const [mediaData] = includes.media;
+    const [user] = includes.users;
+
     return {
-      postId: input.id,
-      text: input.text,
-      likesCount: input.public_metrics.like_count,
-      impressionCount: input.public_metrics.impression_count,
-      createdAt: new Date(input.created_at),
+      postId: tweet.id,
+      text: tweetText,
+      likesCount: tweet.public_metrics.like_count,
+      impressionCount: tweet.public_metrics.impression_count,
+      mediaUrl: mediaData?.url,
+      user: {
+        name: user?.name ?? '',
+        profileImageUrl: user?.profile_image_url ?? '',
+        username: user?.username ?? '',
+      },
+
+      createdAt: new Date(tweet.created_at),
     };
   }
 }
--- src/twitter/infrastructure/mappers/types/tweet.type.ts
+++ src/twitter/infrastructure/mappers/types/tweet.type.ts
@@ -1,4 +1,4 @@
-type PublicMetrics = {
+type PublicMetricsData = {
   retweet_count: number;
   reply_count: number;
   like_count: number;
@@ -8,8 +8,25 @@ type PublicMetrics = {
 };
 
 export type Tweet = {
-  public_metrics: PublicMetrics;
+  public_metrics: PublicMetricsData;
   text: string;
   id: string;
   created_at: string;
+};
+
+type TweetMediaData = {
+  media_key: string;
+  type: string;
+  url: string;
+};
+
+type TweetUserData = {
+  profile_image_url: string;
+  name: string;
+  username: string;
+};
+
+export type TweetIncludes = {
+  media: TweetMediaData[];
+  users: TweetUserData[];
 };
