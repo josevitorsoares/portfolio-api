3d680d276377e4744fe4406e1250f30b212e7d82
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2026-03-02 20:59:40 -0300

    remove: 🗑️ delete all cron and queue structure

--- src/shared/application/contracts/index.ts
+++ src/shared/application/contracts/index.ts
@@ -1,3 +1,1 @@
-export * from './cron.service.interface';
 export * from './http.service.interface';
-export * from './queue.service.interface';
--- src/shared/application/contracts/queue/constants/jobs.constants.ts
+++
@@ -1,1 +0,0 @@
-export const ADD_LAST_TWEET_JOB = 'add-last-tweet-job' as const;
