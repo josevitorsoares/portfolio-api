2475448291881d85391962bb37a83064435a7f7e
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2026-03-02 21:01:58 -0300

    refactor: ♻️ change import path

--- src/shared/main/factories/infrastructure/make-axios-http.service.factory.ts
+++ src/shared/main/factories/infrastructure/make-axios-http.service.factory.ts
@@ -1,6 +1,6 @@
 import type { IHttpService } from '@shared/application/contracts';
 import { axiosInstance } from '@shared/infrastructure/http/axios';
-import { AxiosHttpService } from '@shared/infrastructure/services/axios-http.service';
+import { AxiosHttpService } from '@shared/infrastructure/services';
 
 export const makeAxiosHttpService = (): IHttpService => {
   return new AxiosHttpService(axiosInstance);
