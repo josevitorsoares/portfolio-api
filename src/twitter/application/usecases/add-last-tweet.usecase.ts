20ffd754ffebf6b1f49f24077430f1db228d5357
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2026-03-03 15:11:50 -0300

    feat: ✨ use new tweet input data

--- src/twitter/application/usecases/add-last-tweet.usecase.ts
+++ src/twitter/application/usecases/add-last-tweet.usecase.ts
@@ -1,19 +1,11 @@
-import { TWITTER_ACCOUNT_ID } from '@configs/environment';
-import type { ITwitterGateway } from '../contracts/gateways';
 import type { ITweetRepository } from '../contracts/repositories';
 import type { IAddLastTweetUseCase } from '../contracts/usecases';
+import type { TweetInputDTO } from '../dtos';
 
 export class AddLastTweetUseCase implements IAddLastTweetUseCase {
-  constructor(
-    private readonly _twitterGateway: ITwitterGateway,
-    private readonly _tweetRepository: ITweetRepository,
-  ) {}
+  constructor(private readonly _tweetRepository: ITweetRepository) {}
 
-  async execute(): Promise<void> {
-    const lastTweet = await this._twitterGateway.fetchLastTweets(TWITTER_ACCOUNT_ID);
-
-    if (lastTweet) {
-      await this._tweetRepository.saveLastTweet(lastTweet);
-    }
+  async execute(input: TweetInputDTO): Promise<void> {
+    await this._tweetRepository.saveLastTweet(input);
   }
 }
--- src/twitter/main/factories/usecases/make-add-last-tweet.usecase.factory.ts
+++ src/twitter/main/factories/usecases/make-add-last-tweet.usecase.factory.ts
@@ -1,10 +1,9 @@
 import type { IAddLastTweetUseCase } from '@twitter/application/contracts/usecases';
 import { AddLastTweetUseCase } from '@twitter/application/usecases';
-import { makeRedisTweetRepository, makeTwitterGateway } from '../infrastructure';
+import { makeRedisTweetRepository } from '../infrastructure';
 
 export const makeAddLastTweetUseCase = (): IAddLastTweetUseCase => {
-  const twitterApiGateway = makeTwitterGateway();
   const tweetRepository = makeRedisTweetRepository();
 
-  return new AddLastTweetUseCase(twitterApiGateway, tweetRepository);
+  return new AddLastTweetUseCase(tweetRepository);
 };
