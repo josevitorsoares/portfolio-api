cf7b04a42432ed494fcc2530c492a625186e9072
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2026-03-03 15:09:01 -0300

    refactor: ♻️ remove properties unused

--- src/twitter/domain/entities/twitter.entity.ts
+++ src/twitter/domain/entities/twitter.entity.ts
@@ -1,15 +1,5 @@
-type TweetUser = {
-  profileImageUrl: string;
-  name: string;
-  username: string;
-};
-
 export type TweetEntity = {
   postId: string;
   text: string;
-  likesCount: number;
-  impressionCount: number;
-  mediaUrl: string | undefined;
-  user: TweetUser;
   createdAt: Date;
 };
