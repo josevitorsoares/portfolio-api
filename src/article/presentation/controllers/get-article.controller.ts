34192b31e747e64150c81a900f0abfb28e73af4c
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2026-03-10 21:18:09 -0300

    feature: ✨ add get article controller

---
+++ src/article/presentation/controllers/get-article.controller.ts
@@ -0,0 +1,22 @@
+import type { IGetArticleUseCase } from '@article/application/contracts/usecases';
+import { CustomError } from '@shared/application/errors';
+import { ControllerResponse } from '@shared/presentation/controllers';
+import type { IController, ICustomHttpReply } from '@shared/presentation/http';
+
+export class GetArticleController implements IController {
+  constructor(private readonly _getArticleUseCase: IGetArticleUseCase) {}
+
+  async handle(): Promise<ICustomHttpReply> {
+    try {
+      const articleStream = await this._getArticleUseCase.execute();
+
+      return ControllerResponse.file(articleStream, 'application/pdf', 'article.pdf');
+    } catch (error: any) {
+      if (error instanceof CustomError) {
+        return ControllerResponse.customError(error);
+      }
+
+      return ControllerResponse.internalServerError(error.message);
+    }
+  }
+}
