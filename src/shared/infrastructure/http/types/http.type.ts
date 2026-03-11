72a8433045dae763f61f5870f6d750ab3d70afa7
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2026-03-10 21:13:16 -0300

    feat: ✨ add response type

--- src/shared/infrastructure/http/types/http.type.ts
+++ src/shared/infrastructure/http/types/http.type.ts
@@ -1,8 +1,18 @@
 export type HttpRouteMethods = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
 
+type HttpResponseType =
+  | 'arraybuffer'
+  | 'blob'
+  | 'document'
+  | 'json'
+  | 'text'
+  | 'stream'
+  | 'formdata';
+
 export type HttpServiceRequest<Body = unknown> = {
   method: HttpRouteMethods;
   url: string;
+  responseType?: HttpResponseType | undefined;
   body?: Body | undefined;
   headers?: Record<string, string>;
 };
